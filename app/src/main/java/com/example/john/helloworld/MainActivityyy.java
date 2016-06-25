a10ce46fceb78455c8e552d326600203662810dc
5537github0com <hshahamatdoost@yahoo.com>
2016-06-25 09:14:20 +0430

    Initial commit

---
+++ app/src/main/java/com/example/john/helloworld/MainActivityyy.java
@@ -0,0 +1,13 @@
+package com.example.john.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityyy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainnn);
+    }
+}
